f0b3368a3b975d9e5d2137fcfaa30ea349dd33ff
Michael Havens <michael.s.havens@gmail.com>
2021-03-17 22:50:21 -0400

    initial commit

---
+++ MagnificentPlaceholder/Controllers/Search.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MagnificentPlaceholder.Controllers
+{
+    public class Search : Controller
+    {
+        [HttpPost]
+        public ActionResult PerformSearch(string searchText)
+        {
+            // Demonstrating reaching out to an api serverside
+            return View("~/SearchResults");
+        }
+    }
+}
